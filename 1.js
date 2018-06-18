39cd481eac28f814fc29a2f8d4b7c4992aa05a25
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-06-17 21:19:50 -0400

    feat: Initial commit

---
+++ 1.js
@@ -0,0 +1,13 @@
+var sortByStrings = function(s,t) {
+  
+  var sortedString = ('');
+  for ( var i = 0; i < t.length; ++i) {
+    var checkIndexOfY = s.match(t[i]); 
+    var checkIndexOfX = t.match(s[i]);
+
+    var arrayObjects = (Object.values(checkIndexOfX))
+    console.log(arrayObjects.sort())
+  }
+}
+
+sortByStrings("weather", "therapyw");
